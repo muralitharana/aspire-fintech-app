dd7d889051213f2b244609638c19e1f77d3f90e9
Muralitharan A <muralitharan0002@gmail.com>
2025-04-16 19:45:36 +0530

    chore: toaster and progress bar packages added

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import AppNavigator from './src/navigations/AppNavigator';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
-
+import Toast from 'react-native-toast-message';
 const App = () => {
   return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+      <Toast />
+    </>
   );
 };
 
