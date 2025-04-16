a6b317a50e03e4b84008de83acb9e2261ae6983a
Muralitharan A <muralitharan0002@gmail.com>
2025-04-16 22:14:57 +0530

    fix: android emulator local ip

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,9 +4,12 @@ import {
   DebitCardType,
   WeeklyDebitLimitType,
 } from '../types/debitCardTypes';
+import {Platform} from 'react-native';
 
+// usally baseurl should comes from .env / multi-flavour env setup
 const api = axios.create({
-  baseURL: 'http://localhost:3000/', // Replace with your machine IP for mobile
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:3000/' : 'http://10.0.2.2:3000/', // Replace with your machine IP for mobile
 });
 
 // Optional: Global error handler
